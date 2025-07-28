0b9d4b93ddb4bdf7b27c3800e3a73efd6a60b553
haruto <haruto_hym@icloud.com>
2025-07-28 14:47:46 +0900

    html削除

--- src/main/java/com/example/echoserver/EchoController.java
+++ src/main/java/com/example/echoserver/EchoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class EchoController {
 
+    @CrossOrigin(origins = "*")  
     @GetMapping("/echo")
     public String echo(@RequestParam String text) {
         return text;
